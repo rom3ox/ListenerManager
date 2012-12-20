cfd2d386d2a9afc5b0e61d8080fc4f34a271ea7e
rom3ox <rom3ox@gmail.com>
2012-12-20 15:53:02 +0100

    cambios memores

--- ListenerManager/src/com/github/rom3ox/ListenerManager/ListenerManager.java
+++ ListenerManager/src/com/github/rom3ox/ListenerManager/ListenerManager.java
@@ -1,9 +1,10 @@
 package com.github.rom3ox.ListenerManager;
 
 import java.util.logging.Logger;
-
 import org.bukkit.Server;
 import org.bukkit.configuration.Configuration;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,7 +13,9 @@ public class ListenerManager extends JavaPlugin {
 	public Logger log = this.getLogger();
 	public Server server = this.getServer();
 	public PluginManager pluginManager = this.server.getPluginManager();
+	public Plugin[] Plugins = pluginManager.getPlugins();
 	public Configuration config;
+	public Plugin pJobs = pluginManager.getPlugin("Jobs");
 	
 	@Override 
 	public void onEnable(){
@@ -23,6 +26,8 @@ public class ListenerManager extends JavaPlugin {
 		//pluginManager.registerEvents(new BTPPlayerListener(), this);
 		// Test comment to test GitHub Commits
 	
+		//HandlerList.unregisterAll(listener); // unregister listener from any events it is registered for
+		BlockBreakEvent.getHandlerList().unregister(pJobs);
 	}
 	
 	@Override 
